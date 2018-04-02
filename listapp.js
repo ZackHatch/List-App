48991a168a5adaf149341f3d352efb5011ab4e98
Zachary B Hatch <zh86549@srcs.loc>
2018-04-02 11:42:20 -0700

    yeah

--- listapp.js
+++ listapp.js
@@ -4,24 +4,13 @@ button.addEventListener('click', additem)
 function additem() {
     var input = document.getElementById("item").value;
     var li = document.createElement("li");
-
-
-
     var deletebutton = document.createElement("button");
-
     deletebutton.textContent = "Delete";
     deletebutton.className = "delete"
-
     deletebutton.addEventListener('click', () => {
-        var kyle = deletebutton.perentElement.remove();
+         deletebutton.parentElement.remove();
     });
-
     li.textContent = input + "";
-//    li.appendChild("ul");
     li.appendChild(deletebutton);
-
-
-
     return document.getElementById("ul").appendChild(li);
-
 }
